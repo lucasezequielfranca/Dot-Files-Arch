cbc21ff383d96847ef0c77b0051262c489504b98
Lucas Franca <127765876+lucasezequielfranca@users.noreply.github.com>
2025-07-27 23:56:27 -0300

    Added dot files and install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo pacman -Syu
+
+sudo pacman -S git man tldr kitty rofi dunst fastfetch awesome-terminal-fonts python-pywal
+
+git config --global user.name "Lucas Franca"
+git config --global user.email 127765876+lucasezequielfranca@users.noreply.github.com
+git config --global init.defaultBranch main
+git config --global pull.rebase false
+ssh-keygen -t ed25519
+cat ~/.ssh/id_ed25519.pub
+
+sudo pacman -S --needed git base-devel
+mkdir ~/downloads
+cd downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+
+yay -S google-chrome
+yay -S bumblebee-status
+
+
+
+wal -i ~/.config/wallpapers/wallpaper.jpg
+
+
+sudo cp -r .config/ ~/.config/
+sudo cp .bashrc ~/.bashrc
